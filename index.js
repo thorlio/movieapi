b84a8b9f239e4ac00c194395640f5cd0b8e6b43a
thorlio <thorlio3@gmail.com>
2025-03-03 23:53:02 -0800

    updated Cors sites

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ mongoose
 const allowedOrigins = [
   "http://localhost:1234",
   "http://localhost:3000",
-  "https://www.flixandchill-client.netlify.app",
+  "https://ohmyflix-1cea4b4ad120.herokuapp.com",
   "https://www.movieapi-client.netlify.app",
 ];
 
